b480f713370bc26cb72c22f703f29ff8b987cf32
Sarathkumarms <68678477+sevendreamzone1@users.noreply.github.com>
2025-06-30 11:20:48 +0530

    Dashboard-design

--- src/beolayer/layout/BottomBar.jsx
+++ src/beolayer/layout/BottomBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function BottomBar() {
   return (
     <footer className="h-14 bg-white text-black flex items-center justify-center border-t border-white">
-      <p className="text-sm">© {new Date().getFullYear()} My App</p>
+      <p className="text-sm">© {new Date().getFullYear()} BEO-Software</p>
     </footer>
   );
 }
--- src/beolayer/layout/PageContainer.jsx
+++ src/beolayer/layout/PageContainer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function PageContainer({ title = "Page Content", children }) {
+export default function PageContainer({ title, children }) {
   return (
     <section className="flex-1 p-5 bg-gray-100 overflow-y-auto">
       <h2 className="text-xl font-semibold mb-4">{title}</h2>
