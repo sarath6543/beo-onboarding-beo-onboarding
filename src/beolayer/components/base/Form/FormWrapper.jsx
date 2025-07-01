1b6dd57874464647572f1e9c00d0922e53add0c2
Sarathkumarms <68678477+sevendreamzone1@users.noreply.github.com>
2025-07-01 10:28:34 +0530

    formwrapper

--- src/beolayer/components/base/Form/FormWrapper.jsx
+++ src/beolayer/components/base/Form/FormWrapper.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import "./FormWrapper.css";
 
-const FormWrapper = ({ columns = 2, children }) => {
+const FormWrapper = ({ columns = 2, children, onSave }) => {
   return (
-    <div className={`form-wrapper cols-${columns}`}>
-      {children}
+    <div className={`form-wrapper-container`}>
+      <div className={`form-wrapper cols-${columns}`}>
+        {children}
+      </div>
+
+      {onSave && (
+        <div className="form-wrapper-save">
+          <button
+            onClick={onSave}
+            className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+          >
+            Save
+          </button>
+        </div>
+      )}
     </div>
   );
 };
