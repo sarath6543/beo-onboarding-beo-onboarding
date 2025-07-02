ec59ff0d9a2b5dc667ae0c4bbf02e94ddf59f7e2
aravindka14 <aravindka14@gmail.com>
2025-07-02 09:57:04 +0530

    offer

--- src/pages/Offer/Offer.jsx
+++ src/pages/Offer/Offer.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import PageLayout from "../../beolayer/layout/PageLayout";
 import TopBar from "../../beolayer/layout/TopBar";
 
+import { RiPrinterLine } from "react-icons/ri";
+import { MdOutlineFileDownload } from "react-icons/md";
+
 
 const Offer = () => {
     return (
@@ -10,9 +13,19 @@ const Offer = () => {
 
         <div className="offer_bg flex bg-gray-200 justify-center items-center h-[189px]">
            <div className="">
-                <p className="text-white" style={{fontSize:"46px",fontWeight:"400"}}>Review & Accept Offer</p>
+                <p className="text-white text-5xl" style={{fontWeight:"400"}}>Review & Accept Offer</p>
+                <p className="text-white text-2xl text-center leading-[2]">Your joining date with BEO is</p>
            </div>
         </div>  
+
+        <div className="flex">
+         <button className="flex items-center border px-2 text-lg"><RiPrinterLine />Print</button>
+         <button className="flex items-center border px-2 text-lg"><MdOutlineFileDownload />Download</button>
+        </div>
+
+        <div>
+         
+        </div>
 
        </>
     );
