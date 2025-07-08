8f5158b950393ad13d2e663df776438e9b626a23
Sarathkumarms <68678477+sevendreamzone1@users.noreply.github.com>
2025-07-08 11:13:54 +0530

    page_layout

--- src/beolayer/layout/PageContainer.jsx
+++ src/beolayer/layout/PageContainer.jsx
@@ -1,19 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function PageContainer({ title, children }) {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
   return (
     <section className="flex-1 p-5 bg-[#E6E6E6] overflow-y-auto min-h-0 h-full">
       <div className="flex items-center justify-between mb-4">
-     
         <h2 className="text-xl font-semibold">{title}</h2>
 
- 
-        <div className="text-sm text-gray-600">
-          <Link to="/" className="hover:underline text-black">Home</Link>
-          <span className="mx-2">/</span>
-          <span className="text-black">{title}</span>
-        </div>
+        {!isHomePage && (
+          <div className="text-sm text-gray-600">
+            <Link to="/" className="hover:underline text-black">Home</Link>
+            <span className="mx-2">/</span>
+            <span className="text-black">{title}</span>
+          </div>
+        )}
       </div>
 
       {children}
