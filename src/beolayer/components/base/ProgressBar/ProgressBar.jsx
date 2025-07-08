079879dc5ebd5980704968de8071117f24e7282c
aravindka14 <aravindka14@gmail.com>
2025-07-08 11:45:29 +0530

    educational details updated

--- src/beolayer/components/base/ProgressBar/ProgressBar.jsx
+++ src/beolayer/components/base/ProgressBar/ProgressBar.jsx
@@ -4,7 +4,7 @@ const ProgressBar = (props) => {
     const { completed } = props;
   return (
     <>
-    <div class="h-3 w-full bg-[#EBBC00A1]  overflow-hidden">
+    <div className="h-3 w-full bg-[#EBBC00A1]  overflow-hidden">
         <div style={{width:`${completed}%`}} className="h-full bg-[#00C792] text-right">
             
             
--- src/pages/BackGroundVerification/EducationDetails/forms/EducationDetailsForm.jsx
+++ src/pages/BackGroundVerification/EducationDetails/forms/EducationDetailsForm.jsx
@@ -2,6 +2,12 @@ import React, { useState } from 'react'
 import FormWrapper from '../../../../beolayer/components/base/Form/FormWrapper'
 import InputField from '../../../../beolayer/components/base/InputField/InputField';
 
+const educationModeOptions = [
+    {key:"Online",value:"online"},
+    {key:"Offline",value:"offline"},
+]
+
+
 const EducationDetailsForm = () => {
 
     const [formDataList,setFormDataList] = useState([
@@ -11,7 +17,9 @@ const EducationDetailsForm = () => {
         percentage:"",
         fromDate:"",
         toDate:"",
-        key:"10th Standard"
+        specialization:'',
+        modeOfEducation:"",
+        key:"10th"
         },
     ]);
 
@@ -25,6 +33,7 @@ const EducationDetailsForm = () => {
         percentage: "",
         fromDate: "",
         toDate: "",
+        specialization:'',
         key: value
         }
     ]);
@@ -54,12 +63,14 @@ const EducationDetailsForm = () => {
 
   return (
     <FormWrapper columns={1} onSave={handleSave}> 
-       {formDataList.map((formData,index)=>(
+       {formDataList.map((formData,index)=>{
+        const list = formData.key === "10th" || formData.key === "12th"
+        return (
         <>
             <p className='text-xl font-medium'>{formData.key}</p>
                 <FormWrapper columns={3}>    
                     <InputField
-                        label="Board"
+                        label={list ? "board" : "university"}
                         type="text"
                         value={formData.board}
                         onChange={(e) => handleInputChange(e, index)}
@@ -67,38 +78,72 @@ const EducationDetailsForm = () => {
                         asterisk
                     />
                     <InputField
-                        label="School"
+                        label={list ? "School" : "College/Institute"}
                         type="text"
                         value={formData.school}
                         onChange={(e) => handleInputChange(e, index)}
                         name="school"
                         asterisk
                     />
-                    <InputField
-                        label="Percentage"
-                        type="text"
-                        value={formData.percentage}
-                        onChange={(e) => handleInputChange(e, index)}
-                        name="percentage"
-                        asterisk
-                    />
-                
-                    <InputField
-                        label="From Date"
-                        type="date"
-                        value={formData.fromDate}
-                        onChange={(e) => handleInputChange(e, index)}
-                        name="fromDate"
-                        asterisk
-                    />
-                    <InputField
-                        label="To Date"
-                        type="date"
-                        value={formData.toDate}
-                        onChange={(e) => handleInputChange(e, index)}
-                        name="toDate"
-                        asterisk
-                    /> 
+                    {list ?
+                    ( 
+                    <>
+                        <InputField
+                            label="Percentage"
+                            type="text"
+                            value={formData.percentage}
+                            onChange={(e) => handleInputChange(e, index)}
+                            name="percentage"
+                            asterisk
+                        />
+                        <InputField
+                            label="From Date"
+                            type="date"
+                            value={formData.fromDate}
+                            onChange={(e) => handleInputChange(e, index)}
+                            name="fromDate"
+                            asterisk
+                        />
+                        <InputField
+                            label="To Date"
+                            type="date"
+                            value={formData.toDate}
+                            onChange={(e) => handleInputChange(e, index)}
+                            name="toDate"
+                            asterisk
+                        /> 
+                    </>
+                    )
+                :
+                    (
+                    <>
+                        <InputField
+                            label="Mode of Education"
+                            type="dropdown"
+                            value={formData.modeOfEducation}
+                            onChange={(e) => handleInputChange(e, index)}
+                            name="modeOfEducation"
+                            options={educationModeOptions}
+                            asterisk
+                        />
+                        <InputField
+                            label="Specialization"
+                            type="text"
+                            value={formData.specialization}
+                            onChange={(e) => handleInputChange(e, index)}
+                            name="specialization"
+                            asterisk
+                        />
+                        <InputField
+                            label="Percentage"
+                            type="text"
+                            value={formData.percentage}
+                            onChange={(e) => handleInputChange(e, index)}
+                            name="percentage"
+                            asterisk
+                        /> 
+                    </>
+                    )}
                     <InputField
                         label="Certificate"
                         type="upload"
@@ -134,7 +179,7 @@ const EducationDetailsForm = () => {
 
              <hr />    
         </>
-        ))}
+       )})}
 
             <div className="flex justify-start">     
             <select 
@@ -150,9 +195,6 @@ const EducationDetailsForm = () => {
                 <option disabled={disableDropdown("Others")} value="Others">Others</option>
             </select>
             </div>
-
-
-
     </FormWrapper>
   )
 }
