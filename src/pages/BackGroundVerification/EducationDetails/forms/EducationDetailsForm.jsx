004f4c74c4d0e55de61ed766bb79c69333fffbff
aravindka14 <aravindka14@gmail.com>
2025-07-09 17:20:19 +0530

    Update EducationDetailsForm.jsx
    error in input field

--- src/pages/BackGroundVerification/EducationDetails/forms/EducationDetailsForm.jsx
+++ src/pages/BackGroundVerification/EducationDetails/forms/EducationDetailsForm.jsx
@@ -14,7 +14,7 @@ const EducationDetailsForm = () => {
 
     const { educationList, setEducationList } = useEducationStore();
 
-    const { register ,control , handleSubmit, watch, formState: { errors } } = useForm({
+    const { register ,control , handleSubmit, formState: { errors }, } = useForm({
         defaultValues:{
             education : educationList
         },
@@ -22,12 +22,14 @@ const EducationDetailsForm = () => {
 
     const { fields, append, remove } = useFieldArray({
     control,
-    name: 'education'
+    name: "education",
   });
 
   const onSubmit = (data) => {
     console.log("Saving Education Details:", data.education);
-    setExperienceList(data.education);
+    setEducationList(data.education);
+    
+    
   };
 
     const handleSelectChange = (e) => {
@@ -55,142 +57,166 @@ const EducationDetailsForm = () => {
     const [selectDrop,setSelectDrop] = useState("")
 
   return (
-    <FormWrapper columns={1} onSubmit={handleSubmit(onSubmit)}> 
-       {fields.map((field,index)=>{
-        const list = field.key === "10th Standard" || field.key === "12th Standard"
-        return (
-        <>
-            <p className='text-xl font-medium'>{field.key}</p>
-                <FormWrapper columns={3}>    
-                    <InputField
-                        label={list ? "board" : "university"}
-                        type="text"
-                        // value={formData.board}
-                        // onChange={(e) => handleInputChange(e, index)}
-                        name="board"
-                        asterisk
-                    />
-                    <InputField
-                        label={list ? "School" : "College/Institute"}
-                        type="text"
-                        // value={formData.school}
-                        // onChange={(e) => handleInputChange(e, index)}
-                        name="school"
-                        asterisk
-                    />
-                    {list ?
-                    ( 
-                    <>
-                        <InputField
-                            label="Percentage"
-                            type="text"
-                            // value={formData.percentage}
-                            // onChange={(e) => handleInputChange(e, index)}
-                            name="percentage"
-                            asterisk
-                        />
-                        <InputField
-                            label="From Date"
-                            type="date"
-                            // value={formData.fromDate}
-                            // onChange={(e) => handleInputChange(e, index)}
-                            name="fromDate"
-                            asterisk
-                        />
-                        <InputField
-                            label="To Date"
-                            type="date"
-                            // value={formData.toDate}
-                            // onChange={(e) => handleInputChange(e, index)}
-                            name="toDate"
-                            asterisk
-                        /> 
-                    </>
-                    )
-                :
-                    (
-                    <>
-                        <InputField
-                            label="Mode of Education"
-                            type="dropdown"
-                            // value={formData.modeOfEducation}
-                            // onChange={(e) => handleInputChange(e, index)}
-                            name="modeOfEducation"
-                            options={educationModeOptions}
-                            asterisk
-                        />
-                        <InputField
-                            label="Specialization"
-                            type="text"
-                            // value={formData.specialization}
-                            // onChange={(e) => handleInputChange(e, index)}
-                            name="specialization"
-                            asterisk
-                        />
-                        <InputField
-                            label="Percentage"
-                            type="text"
-                            // value={formData.percentage}
-                            // onChange={(e) => handleInputChange(e, index)}
-                            name="percentage"
-                            asterisk
-                        /> 
-                    </>
-                    )}
-                    <InputField
-                        label="Certificate"
-                        type="upload"
-                        // value={formData.toDate}
-                        // onChange={(e) => handleInputChange(e, index)}
-                        name=""
-                        asterisk
-                    />  
-                </FormWrapper>
-               
-                {fields.length > 1 &&
-                    <>
-                        <div className="flex justify-end">
-                        <button
-                            className="px-4 py-2 rounded transition-colors duration-300 text-base bg-red-200 hover:bg-red-500 hover:text-white" 
-                            // onClick={()=>handleDeleteForm(index)}
-                            onClick={() => remove(index)}
-                        >Delete -</button>
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <FormWrapper columns={1} > 
+           {fields.map((field,index)=>{
+            const isSchoolLevel = field.key === "10th Standard" || field.key === "12th Standard"
+            return (
+                <div key={field.id}>
+                    <p className='text-xl font-medium'>{field.key}</p>
+                        <FormWrapper columns={3}>    
+                            <InputField
+                                label={isSchoolLevel ? "Board" : "University"}
+                                type="text"
+                                {...register(`education.${index}.board`, {
+                                    required: `${isSchoolLevel ? "Board":"University"} is required`,
+                                })}
+                                error={errors.education?.[index]?.board?.message}
+                                name="board"
+                                asterisk
+                            />
+                            <InputField
+                                label={isSchoolLevel ? "School" : "College/Institute"}
+                                type="text"
+                                {...register(`education.${index}.school`,{
+                                    required :`${isSchoolLevel ? "School" : "College/Institute"} is required`
+                                })}
+                                error={errors?.education?.[index]?.school?.message}
+                              name="school"
+                                asterisk
+                            />
+                            {isSchoolLevel ?
+                            ( 
+                            <>
+                                <InputField
+                                    label="Percentage"
+                                    type="text"
+                                    {...register(`education.${index}.percentage`,{
+                                        required :`Percentage is required`
+                                    })}
+                                    error={errors?.education?.[index]?.percentage?.message}
+                                    name="percentage"
+                                    asterisk
+                                />
+                                <InputField
+                                    label="From Date"
+                                    type="date"
+                                    {...register(`education.${index}.fromDate`,{
+                                        required :`From date is required`
+                                    })}
+                                    error={errors?.education?.[index]?.fromDate?.message}
+                                    name="fromDate"
+                                    asterisk
+                                />
+                                <InputField
+                                    label="To Date"
+                                    type="date"
+                                    {...register(`education.${index}.toDate`,{
+                                        required :`To date is required`
+                                    })}
+                                    error={errors?.education?.[index]?.toDate?.message}
+                                    name="toDate"
+                                    asterisk
+                                /> 
+                            </>
+                            )
+                        :
+                            (
+                            <>
+                                <InputField
+                                    label="Mode of Education"
+                                    type="dropdown"
+                                    {...register(`education.${index}.modeOfEducation`,{
+                                        required :`Mode of Education is required`
+                                    })}
+                                    error={errors?.education?.[index]?.modeOfEducation?.message}
+                                    name="modeOfEducation"
+                                    options={educationModeOptions}
+                                    asterisk
+                                />
+                                <InputField
+                                    label="Specialization"
+                                    type="text"
+                                    {...register(`education.${index}.specialization`,{
+                                        required :`Specialization is required`
+                                    })}
+                                    error={errors?.education?.[index]?.specialization?.message}
+                                    name="specialization"
+                                    asterisk
+                                />
+                                <InputField
+                                    label="Percentage"
+                                    type="text"
+                                    {...register(`education.${index}.precentage`,{
+                                        required :`Specialization is required`
+                                    })}
+                                    error={errors?.education?.[index]?.precentage?.message}
+                                    name="precentage"
+                                    asterisk
+                                /> 
+                            </>
+                            )}
+                            <InputField
+                                label="Certificate"
+                                type="upload"
+                                {...register(`education.${index}.precentage`,{
+                                        required :`Specialization is required`
+                                    })}
+                                error={errors?.education?.[index]?.precentage?.message}
+                                name=""
+                                asterisk
+                            />  
+                        </FormWrapper>
+                       
+                        {fields.length > 1 &&
+                            <>
+                                <div className="flex justify-end">
+                                <button
+                                    className="px-4 py-2 rounded transition-colors duration-300 text-base bg-red-200 hover:bg-red-500 hover:text-white" 
+                                    onClick={() => remove(index)}
+                                >Delete -</button>
+                                </div>
+                            </>  
+                        }
+        
+                        <div className="my-4 me-6 flex justify-end">
+                            <label className="flex items-center space-x-2">
+                                <span className="text-sm">Click if This is your highest education qualification</span>
+                                <input
+                                    className="w-4 h-5"
+                                    type="checkbox"
+                                />
+                            </label>
                         </div>
-                    </>  
-                }
+                     <hr /> 
+                </div>   
+        
+           )})}
+    
+                <div className="flex justify-between">     
+                <select 
+                    onChange={handleSelectChange}
+                    value={selectDrop}
+                    className="mt-1 block px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                >
+                    <option value="" disabled hidden>Add Education</option>
+                    <option hidden={disableDropdown("10th Standard")} value="10th Standard">10th standard</option>
+                    <option hidden={disableDropdown("12th Standard")} value="12th Standard">12th standard</option>
+                    <option value="Diploma">Diploma</option>
+                    <option value="UG">UG</option>
+                    <option value="PG">PG</option>
+                    <option value="Others">Others</option>
+                </select>
 
-                <div className="my-4 me-6 flex justify-end">
-                    <label className="flex items-center space-x-2">
-                        <span className="text-sm">Click if This is your highest education qualification</span>
-                        <input
-                        className="w-4 h-5"
-                        type="checkbox"
-                        // checked={}
-                        // onChange={}
-                        />
-                    </label>
+                <button
+                    type="submit"
+                    className="bg-[#DADADA] text-black px-4 py-2 rounded hover:bg-black hover:text-white transition-colors duration-300 text-base"
+                    >
+                    Save
+                </button>
                 </div>
-
-             <hr />    
-        </>
-       )})}
-
-            <div className="flex justify-start">     
-            <select 
-                onChange={handleSelectChange}
-                value={selectDrop}
-                className="mt-1 block px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-            >
-                <option value="" disabled hidden>Add Education</option>
-                <option hidden={disableDropdown("10th Standard")} value="10th Standard">10th standard</option>
-                <option hidden={disableDropdown("12th Standard")} value="12th Standard">12th standard</option>
-                <option value="Diploma">Diploma</option>
-                <option value="UG">UG</option>
-                <option value="PG">PG</option>
-                <option value="Others">Others</option>
-            </select>
-            </div>
-    </FormWrapper>
+        </FormWrapper>
+    </form>
   )
 }
 
